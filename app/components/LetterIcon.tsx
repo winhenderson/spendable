5b82abb7c21f623b9d62c73bbfd47b8bf7ddf409
Winfield Henderson <109438943+winhenderson@users.noreply.github.com>
2024-01-17 14:08:56 -0500

    made default icon thing

--- app/components/LetterIcon.tsx
+++ app/components/LetterIcon.tsx
@@ -11,40 +11,49 @@ type Props = {
 const LetterIcon: React.FC<Props> = ({ title }) => {
   const [colorScheme] = React.useContext(ColorSchemeContext);
   const bgColor = randomColor({
-    format: "rgb",
     luminosity: colorScheme === "dark" ? "dark" : "bright",
     seed: title.split("")[0],
   });
-  const rgbValues = bgColor.match(/\d+/g)?.map((value) => parseInt(value));
-  console.log(bgColor);
 
-  // Ensure we have three valid RGB values
-  if (!rgbValues) {
-    throw new Error("Invalid RGB background color format");
-  }
+  const textColor = getContrast(bgColor);
 
-  // Calculate the luminance of the background color using the RGB values
-  const luminance =
-    0.299 * rgbValues[0] + 0.587 * rgbValues[1] + 0.114 * rgbValues[2];
-
-  // Determine the contrast ratios between the background color and black and white text
-  const contrastRatioBlack = (luminance + 0.05) / 0.05;
-  const contrastRatioWhite = (1.0 - luminance + 0.05) / 0.05;
-
-  const textColor =
-    contrastRatioBlack > contrastRatioWhite ? "zinc-900" : "zinc-50";
-
-  console.log(
-    "tw:",
-    tw`bg-[${bgColor}] rounded-full flex justify-center items-center p-1 w-7 h-7 text-${textColor}`
-  );
   return (
     <View
-      style={tw`bg-[${bgColor}] rounded-full flex justify-center items-center p-1 w-7 h-7 text-${textColor}`}
+      style={tw`bg-[${bgColor}] rounded-full flex justify-center items-center p-1 w-7 h-7`}
     >
-      <Text>{title.split("")[0]}</Text>
+      <Text style={tw`text-${textColor} font-semibold`}>
+        {title.split("")[0]}
+      </Text>
     </View>
   );
 };
+
+function getContrast(hexcolor: string) {
+  // If a leading # is provided, remove it
+  if (hexcolor.slice(0, 1) === "#") {
+    hexcolor = hexcolor.slice(1);
+  }
+
+  // If a three-character hexcode, make six-character
+  if (hexcolor.length === 3) {
+    hexcolor = hexcolor
+      .split("")
+      .map(function (hex) {
+        return hex + hex;
+      })
+      .join("");
+  }
+
+  // Convert to RGB value
+  const r = parseInt(hexcolor.substr(0, 2), 16);
+  const g = parseInt(hexcolor.substr(2, 2), 16);
+  const b = parseInt(hexcolor.substr(4, 2), 16);
+
+  // Get YIQ ratio
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+
+  // Check contrast
+  return yiq >= 128 ? "zinc-900" : "zinc-50";
+}
 
 export default LetterIcon;
