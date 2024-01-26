689ef00d5287c3a1f807fe6289b2b9bc8702f7ec
Winfield Henderson <109438943+winhenderson@users.noreply.github.com>
2024-01-26 11:29:19 -0500

    wip: started making bank screen a bit better"
    "

--- app/screens/Banks.tsx
+++ app/screens/Banks.tsx
@@ -11,6 +11,7 @@ import {
 } from "../lib";
 import Loading from "../components/Loading";
 import UserContext from "../UserContext";
+import LetterIcon from "../components/LetterIcon";
 
 const Banks: React.FC = () => {
   const [user, setUser] = useContext(UserContext);
@@ -45,52 +46,63 @@ const Banks: React.FC = () => {
   }
 
   return (
-    <SafeAreaView
-      style={tw`bg-white dark:bg-zinc-900 items-center justify-center flex grow p-1 gap-2`}
-    >
+    <SafeAreaView style={tw`bg-white dark:bg-zinc-900 grow`}>
       <FlatList
+        ListHeaderComponent={
+          <View style={tw`flex items-center gap-4`}>
+            <View style={tw`flex flex-row justify-center items-center`}>
+              <Text style={tw`text-3xl text-teal-900 font-bold`}>
+                Connected Banks
+              </Text>
+            </View>
+
+            <PlaidLink
+              tokenConfig={{
+                token: linkToken,
+                noLoadingState: false,
+              }}
+              onSuccess={async (success: LinkSuccess) => {
+                await publicTokenExchange(success.publicToken, user.id);
+                const res = await getAllTransactions(user.id);
+
+                if (!res.ok) {
+                  Alert.alert(
+                    "Database error",
+                    `failed to fetch transactions for new bank.\nError: ${res.error}`
+                  );
+                  return;
+                }
+
+                setUser({
+                  ...user,
+                  transactions: [...user.transactions, ...res.value],
+                });
+              }}
+              onExit={(exit: LinkExit) => console.log(exit)}
+            >
+              <Text
+                style={tw`bg-orange-600 p-4 text-white font-bold uppercase overflow-hidden rounded-2xl w-50 text-center`}
+              >
+                Add Account
+              </Text>
+            </PlaidLink>
+          </View>
+        }
         data={banks}
+        contentContainerStyle={tw`gap-2 px-4`}
         renderItem={(bank) => (
-          <View>
-            {bank.item.logo && <Image source={{ uri: bank.item.logo }} />}
+          <View style={tw`flex flex-row gap-2 items-center`}>
+            {bank.item.logo ? (
+              <Image source={{ uri: bank.item.logo }} />
+            ) : (
+              <LetterIcon title={bank.item.name} />
+            )}
             <Text style={tw`text-teal-950 dark:text-teal-200`}>
               {bank.item.name}
             </Text>
           </View>
         )}
       />
-
-      <PlaidLink
-        tokenConfig={{
-          token: linkToken,
-          noLoadingState: false,
-        }}
-        onSuccess={async (success: LinkSuccess) => {
-          await publicTokenExchange(success.publicToken, user.id);
-          const now = new Date();
-          const res = await getAllTransactions(user.id);
-
-          if (!res.ok) {
-            Alert.alert(
-              "Database error",
-              `failed to fetch transactions for new bank.\nError: ${res.error}`
-            );
-            return;
-          }
-
-          setUser({
-            ...user,
-            transactions: [...user.transactions, ...res.value],
-          });
-        }}
-        onExit={(exit: LinkExit) => console.log(exit)}
-      >
-        <Text
-          style={tw`bg-orange-600 p-4 text-white font-bold uppercase overflow-hidden rounded-2xl w-50 text-center`}
-        >
-          Add Account
-        </Text>
-      </PlaidLink>
     </SafeAreaView>
   );
 };
